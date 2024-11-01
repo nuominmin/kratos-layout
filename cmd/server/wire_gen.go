a8aeb59bc5613338707ac663f12782f54a7ed4bd
nuominmin <776191753@qq.com>
2024-11-01 10:55:31 +0800

    feat: Remove redis conf, Remove grpc server

--- cmd/server/wire_gen.go
+++ cmd/server/wire_gen.go
@@ -31,9 +31,8 @@ func wireApp(confServer *conf.Server, confData *conf.Data, logger log.Logger) (*
 	greeterRepo := data.NewGreeterRepo(dataData, logger)
 	greeterUsecase := biz.NewGreeterUsecase(greeterRepo, logger)
 	greeterService := service.NewGreeterService(greeterUsecase)
-	grpcServer := server.NewGRPCServer(confServer, greeterService, logger)
 	httpServer := server.NewHTTPServer(confServer, greeterService, logger)
-	app := newApp(logger, grpcServer, httpServer)
+	app := newApp(logger, httpServer)
 	return app, func() {
 		cleanup()
 	}, nil
