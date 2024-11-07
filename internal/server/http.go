934b1a8bc01ebfed6a3168178d26b4d1f9d3e731
nuominmin <776191753@qq.com>
2024-11-07 11:38:29 +0800

    feat: Add health api

--- internal/server/http.go
+++ internal/server/http.go
@@ -4,9 +4,9 @@ import (
 	pb "github.com/go-kratos/kratos-layout/api"
 	"github.com/go-kratos/kratos-layout/internal/conf"
 	"github.com/go-kratos/kratos-layout/internal/service"
-
 	"github.com/go-kratos/kratos/v2/middleware/recovery"
 	"github.com/go-kratos/kratos/v2/transport/http"
+	nethttp "net/http"
 )
 
 // NewHTTPServer new an HTTP server.
@@ -26,6 +26,9 @@ func NewHTTPServer(c *conf.Server, svc *service.Service) *http.Server {
 		opts = append(opts, http.Timeout(c.Http.Timeout.AsDuration()))
 	}
 	srv := http.NewServer(opts...)
+	srv.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(nethttp.StatusOK)
+	})
 	pb.RegisterV1HTTPServer(srv, svc)
 	return srv
 }
